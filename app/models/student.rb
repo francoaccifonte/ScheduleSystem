922338aec429a2e26f0dc4e567cf249354644ecc
FrancoAccifonte <franco.accifonte@gmail.com>
2022-02-16 20:04:13 -0300

    add students resource

---
+++ app/models/student.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Student < ApplicationRecord
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
